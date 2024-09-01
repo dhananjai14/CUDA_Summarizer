3242a99210c9b8a955e0f0ca1cfb6667c248de4b
Dhananjai Singh <dhananjai.eee@gmail.com>
2024-09-01 11:58:03 +0530

    setup.p file created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'CUDA Summarizer and code generator',
+    version= '1.0',
+    author= 'Dhananjai',
+    author_email= 'dhananjai.eee@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
